e2e5520fea02c77f353df8945d1656218b73a5ba
Clayton Bahia <gabriel808bahia@gmail.com>
2024-09-24 19:04:29 -0300

    cors

--- src/server.js
+++ src/server.js
@@ -1,8 +1,11 @@
 import express from "express";
+import cors from "cors"; 
 import { filmeRouter } from "./routes/filme.routes.js";
 
 const app = express();
 const port = 4000;
+
+app.use(cors());
 
 app.use(express.json());
 app.use(filmeRouter);
